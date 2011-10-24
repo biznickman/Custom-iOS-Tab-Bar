402ed6de449d4ef73e39704872b25de1d018bbc1
Nick O'Neill <mr.nick.oneill@gmail.com>
2011-10-24 13:23:04 -0700

    Initial Commit

---
+++ CustomTabBar/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  CustomTabBar
+//
+//  Created by Nick ONeill on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomTabBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabBar
+//
+//  Created by Nick ONeill on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBAppDelegate class]));
+    }
+}
